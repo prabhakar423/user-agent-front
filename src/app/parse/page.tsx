0c3cd3f3fc6a4dbfb187d083ac1c46fb3d3d8992
Your Name <you@example.com>
2024-10-24 00:16:19 +0530

    Initial commit for Next.js app

--- src/app/parse/page.tsx
+++ src/app/parse/page.tsx
@@ -28,7 +28,7 @@ const ParsePage = () => {
     try {
       // Make the API request to parse the user agent (replace with your API endpoint)
       const response = await axios.get<ParsedData>(
-        "http://jobmela.info:8080/user-agent",
+        "https://jobmela.info:8080/user-agent",
         {
           params: { userAgent: userAgent },
         }
